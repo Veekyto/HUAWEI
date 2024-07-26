b151646c9a561f428acfc547601a930c008826fd
veeky <2274557208@qq.com>
2024-07-26 15:39:06 +0800

    sa slove

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import math
 import random
 import copy
-import numpy as np
-import matplotlib.pyplot as plt
+import time
+# import matplotlib.pyplot as plt
+
 class Task:
     def __init__(self, id, size, output, k,affinitive_machines):
         self.id = id
@@ -31,7 +32,7 @@ class Disk:
         self.speed = speed
         self.quota = quota
         self.empty=quota
-        self.time=0
+        # self.time=0
 
 def load_data():
     
@@ -103,7 +104,7 @@ class TaskGraph:
             return None  
         return result_order
 
-class GA:
+class SA:
     class Individual:
         def __init__(self,num_tasks):
             self.tasks_plan=[-1]*num_tasks
@@ -113,19 +114,22 @@ class GA:
             self.error=0
             self.mode=-1
 
-    def __init__(self, tasks, machines, disks,  pop_size=50, crossover_rate=0.8, mutation_rate=0.2):
+    def __init__(self, tasks, machines, disks,  pop_size=50, init_temperature=1000,cooling_rate=0.95):
         self.tasks = tasks
         self.machines = machines
         self.disks = disks
         self.pop_size = pop_size
-        self.crossover_rate = crossover_rate
-        self.mutation_rate = mutation_rate
+        self.init_temperature=init_temperature
+        self.cooling_rate=cooling_rate
         self.num_tasks = len(tasks)
         self.num_machines = len(machines)
         self.num_disks = len(disks)
         self.record_cost = []
         self.record_error = []
         self.population = self.initialize_population()
+        self.best_solution=copy.deepcopy(self.population[0])
+        self.best_solution.cost = float('inf')
+        
         
         # for individual in self.population:
         #     self.calculate_cost(individual)
@@ -136,17 +140,18 @@ class GA:
         for _ in range(self.pop_size):
             sort_task=TaskGraph(self.tasks)
             disk_empty = [disk.quota for disk in self.disks]
-            individual = GA.Individual(self.num_tasks)
+            individual = SA.Individual(self.num_tasks)
             individual.tasks_plan = sort_task.topological_sort()
             for i in range(self.num_tasks):
                 task=self.tasks[individual.tasks_plan[i]]
                 individual.machines_plan[i] = random.choice(task.affinitive_machines)
-                chosen_disk=random.randint(0,self.num_disks-1)
-                while task.output > disk_empty[chosen_disk]:
-                    chosen_disk = random.randint(0, self.num_disks - 1)
-                    #print("task.output=",task.output,"chosen_disk=",chosen_disk,"disk_empty[chosen_disk]",disk_empty[chosen_disk])
-                individual.disks_plan[i] = chosen_disk
-                disk_empty[chosen_disk] -= task.output
+                individual.disks_plan[i] = random.randint(0,self.num_disks-1)
+                # chosen_disk=random.randint(0,self.num_disks-1)
+                # while task.output > disk_empty[chosen_disk]:
+                #      chosen_disk = random.randint(0, self.num_disks - 1)
+                #      #print("task.output=",task.output,"chosen_disk=",chosen_disk,"disk_empty[chosen_disk]",disk_empty[chosen_disk])
+                # individual.disks_plan[i] = chosen_disk
+                # disk_empty[chosen_disk] -= task.output
             population.append(individual)
             #if _==1:
                 # print("tasks_plan=",individual.tasks_plan)
@@ -154,51 +159,53 @@ class GA:
                 # print("disk_plan=",individual.disks_plan)
         return population
     
-    def select_parents(self):
-        tournament_size = 5
-        tournament = random.sample(self.population, tournament_size)
-        return min(tournament,key=lambda x: x.cost)
+    def neighbor_solution(self,current_solution):
+        new_solution=copy.deepcopy(current_solution)
+        num_tasks = self.num_tasks
+        idx1, idx2 = random.sample(range(num_tasks), 2)
+        mode=random.randint(1,3)
+        if(mode==1):
+            new_solution.tasks_plan[idx1], new_solution.tasks_plan[idx2] = new_solution.tasks_plan[idx2], new_solution.tasks_plan[idx1]
+            new_solution.machines_plan[idx1], new_solution.machines_plan[idx2] = new_solution.machines_plan[idx2], new_solution.machines_plan[idx1]
+            new_solution.disks_plan[idx1], new_solution.disks_plan[idx2] = new_solution.disks_plan[idx2], new_solution.disks_plan[idx1]
+        elif(mode==2):
+            task=self.tasks[new_solution.tasks_plan[idx1]]
+            new_solution.machines_plan[idx1]=random.choice(task.affinitive_machines)
+        elif(mode==3):
+            if(random.random()<=0.5):
+                new_solution.disks_plan[idx1]=random.randint(0,self.num_disks-1)
+            else:
+                new_solution.disks_plan[idx1], new_solution.disks_plan[idx2] = new_solution.disks_plan[idx2], new_solution.disks_plan[idx1]
+        return new_solution
 
-    def crossover(self, parent1, parent2):
-        offspring=copy.deepcopy(parent1)
-        if random.random() < self.crossover_rate:
-            crossover_point = random.randint(1, self.num_tasks - 1)
-            offspring.tasks_plan=parent1.tasks_plan[:crossover_point] + parent2.tasks_plan[crossover_point:]
-            offspring.machines_plan=parent1.machines_plan[:crossover_point] + parent2.machines_plan[crossover_point:]
-            if random.random() < 0.5:
-                offspring.disks_plan=parent1.disks_plan[:crossover_point] + parent2.disks_plan[crossover_point:]
-            offspring.mode=0
-        return offspring
-
-    def mutate(self, solution):
-        if random.random() < self.mutation_rate:
-            if random.random() < 0.5:
-                mutation_point1, mutation_point2 = random.sample(range(self.num_tasks), 2)
-                solution.tasks_plan[mutation_point1], solution.tasks_plan[mutation_point2] = solution.tasks_plan[mutation_point2], solution.tasks_plan[mutation_point1]
-                solution.machines_plan[mutation_point1], solution.machines_plan[mutation_point2] = solution.machines_plan[mutation_point2], solution.machines_plan[mutation_point1]
-            if random.random() < 0.5:
-                mutation_point1, mutation_point2 = random.sample(range(self.num_tasks), 2)
-                solution.disks_plan[mutation_point1], solution.disks_plan[mutation_point2] = solution.disks_plan[mutation_point2], solution.disks_plan[mutation_point1]
-            if random.random() < 0.5:
-                mutation_point = random.randint(0, self.num_tasks - 1)
-                solution.machines_plan[mutation_point] = random.choice(self.tasks[solution.tasks_plan[mutation_point]].affinitive_machines)
-        return solution
     
     def calculate_cost(self,solution):
+        global count1
+        global count2
         finish_time=0
         count=0
         temp_tasks=copy.deepcopy(self.tasks)
         temp_machines=copy.deepcopy(self.machines)
         temp_disks=copy.deepcopy(self.disks)
         for i in range(self.num_tasks):
+            # print("~"*50)
             task=temp_tasks[solution.tasks_plan[i]]
+            # print("task_id=",task.id)
             task.machine_id=solution.machines_plan[i]
+            # print(task.machine_id)
             machine = temp_machines[solution.machines_plan[i]]
+            # print("machine.id=",machine.id," time=",machine.time)
             task.disk_id=solution.disks_plan[i]
+            
             disk=temp_disks[solution.disks_plan[i]]
-            now_time=max(task.a,machine.time,disk.time)
+            # print("disk_id=",disk.id, "quota=",disk.quota)
+            now_time=machine.time
+            for j in task.data_dep:
+                now_time=max(now_time,temp_tasks[j].d)
+            # print("now_time=",now_time)
             task.a=now_time
             spend_time=0
+            # print("task.data_dep=",task.data_dep)
             for j in task.data_dep:
                 
                 spend_time+=round(temp_tasks[j].output/temp_disks[temp_tasks[j].disk_id].speed,0)
@@ -208,7 +215,7 @@ class GA:
             spend_time+=round(task.output/disk.speed,0)
             task.d=now_time+spend_time
             machine.time=task.d
-            disk.time=task.d
+            
             disk.quota-=task.output
             finish_time=max(finish_time,task.d)
         error=self.check_error(temp_tasks,temp_machines,temp_disks)
@@ -225,72 +232,84 @@ class GA:
         for i in tasks:
             for j in i.env_dep:
                 if i.a<tasks[j].c:
-                    env_error+=1
+                    env_error+=abs(i.a-tasks[j].c)
             for j in i.data_dep:
                 if i.a<tasks[j].d:
-                    data_error+=1
+                    data_error+=abs(i.a-tasks[j].d)
         for i in disks:
             if i.quota<0:
                 quota_error+=abs(i.quota)
-        return env_error*3+round(data_error*3,0)+quota_error*2
+        return env_error*150+round(data_error*160,0)+quota_error*150
     
-    def solver(self,max_iteration=200):
-        for i in range(max_iteration):
-            for solution in self.population:
-                self.calculate_cost(solution)
+    def acceptance_probability(self, cost, new_cost, temperature):
+        if new_cost < cost:
+            return 1.0
+        else:
+            return math.exp((cost - new_cost) / temperature)
+
+    def solver(self,max_iteration=1400):
+        current_temperature=self.init_temperature
+        for iteration in range(max_iteration):
             new_population=[]
-            it=0
-            while it<self.pop_size:
-                parent1=self.select_parents()
-                parent2=self.select_parents()
-                child=self.crossover(parent1,parent2)
-                child=self.mutate(child)
-                self.calculate_cost(child)
-                if child.error==0:
-                    new_population.append(child)
-                    it+=1
-                    global count1
-                    global count2
-                    if(child.mode==-1):
-                        count1+=1
-                    else:
-                        count2+=1
+            for solution in self.population:
                 
+                new_solution=self.neighbor_solution(solution)
+                self.calculate_cost(new_solution)
+                self.calculate_cost(solution)
+                if new_solution.error==0 and new_solution.cost<self.best_solution.cost:
+                    self.best_solution=new_solution
+                    
+                if random.random() < self.acceptance_probability(solution.cost, new_solution.cost, current_temperature):
+                    new_population.append(new_solution)
+                   
+                        
+                else:          
+                    new_population.append(solution)
+                    
+            current_temperature=self.cooling_rate*current_temperature
             self.population=new_population
             self.population.sort(key=lambda x: x.cost)
             self.record_cost.append(self.population[0].cost)
             self.record_error.append(self.population[0].error)
-#display
+    def display(self):
+       
         temp_tasks=copy.deepcopy(self.tasks)
         temp_machines=copy.deepcopy(self.machines)
         temp_disks=copy.deepcopy(self.disks)
-        solution=self.population[0]
+        # for solution in self.population:
+        #     if solution.error==0:
+        #         solution=self.population[0]
+        solution=copy.deepcopy(self.best_solution)
         finish_time=0
         for i in range(self.num_tasks):
-            task=temp_tasks[solution.tasks_plan[i]]
-            task.machine_id=solution.machines_plan[i]
-            machine = temp_machines[solution.machines_plan[i]]
-            task.disk_id=solution.disks_plan[i]
-            disk=temp_disks[solution.disks_plan[i]]
-            now_time=max(task.a,machine.time,disk.time)
-            task.a=now_time
-            spend_time=0
-            for j in task.data_dep:
-                
-                spend_time+=round(temp_tasks[j].output/temp_disks[temp_tasks[j].disk_id].speed,0)
-            task.b=now_time+spend_time
-            spend_time+=round(task.size/machine.power,0)
-            task.c=now_time+spend_time
-            spend_time+=round(task.output/disk.speed,0)
-            task.d=now_time+spend_time
-            machine.time=task.d
-            disk.time=task.d
-            disk.quota-=task.output
-            finish_time=max(finish_time,task.d)
-            #print(task.id,task.a,task.machine_id+1,task.disk_id+1)
+                    task=temp_tasks[solution.tasks_plan[i]]
+                    task.machine_id=solution.machines_plan[i]
+                    machine = temp_machines[solution.machines_plan[i]]
+                    task.disk_id=solution.disks_plan[i]
+                    disk=temp_disks[solution.disks_plan[i]]
+                    now_time=machine.time
+                    task.a=now_time
+                    spend_time=0
+                    for j in task.data_dep:
+                        
+                        spend_time+=round(temp_tasks[j].output/temp_disks[temp_tasks[j].disk_id].speed,0)
+                    task.b=now_time+spend_time
+                    spend_time+=round(task.size/machine.power,0)
+                    task.c=now_time+spend_time
+                    spend_time+=round(task.output/disk.speed,0)
+                    task.d=now_time+spend_time
+                    machine.time=task.d
+                    
+                    disk.quota-=task.output
+                    finish_time=max(finish_time,task.d)
+                    #print(task.id,task.a,task.machine_id+1,task.disk_id+1)
         for task in temp_tasks:
             print(task.id,int(task.a),task.machine_id+1,task.disk_id+1)
-        print("finish_time=",finish_time)
+        # print("finish_time=",finish_time)
+        # self.calculate_cost(solution)
+        # print("cost=",solution.cost)
+        # print("error=",solution.error)
+           
         
         
 
@@ -300,6 +319,7 @@ class GA:
 
                 
 def main():
+    start_time=time.time()
 
     # record_cost=[]
     # record_error=[]
@@ -308,29 +328,36 @@ def main():
     global count2 
     count1=0
     count2=0
+    
+
 
     #for i in tasks:
         #print(i.id,i.size,i.output,i.num_machines,i.affinitive_machines)
         #print(i.env_dep,i.data_dep)
 
-    ga_slover=GA(tasks,machines,disks,pop_size=50)
-    ga_slover.solver()  
-    print("cost=",ga_slover.record_cost)
-    print("error=",ga_slover.record_error)
-    print(count1,count2)
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
+    ga_slover=SA(tasks,machines,disks,pop_size=5)
+    ga_slover.solver() 
+    ga_slover.display()
+    end_time=time.time() 
+    # print("count1=",count1)
+    # print("count2=",count2)
+    # print("time=",end_time-start_time)
+    # print("cost=",ga_slover.record_cost)
+    # print("error=",ga_slover.record_error)
+    # print(count1,count2)
+    # fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
 
-    ax1.plot(range(len(ga_slover.record_cost)), ga_slover.record_cost, label='Cost')
-    ax1.set_xlabel('Iteration')
-    ax1.set_ylabel('Cost')
-    ax1.legend()
+    # ax1.plot(range(len(ga_slover.record_cost)), ga_slover.record_cost, label='Cost')
+    # ax1.set_xlabel('Iteration')
+    # ax1.set_ylabel('Cost')
+    # ax1.legend()
 
-    ax2.plot(range(len(ga_slover.record_error)), ga_slover.record_error, label='Error')
-    ax2.set_xlabel('Iteration')
-    ax2.set_ylabel('Error')
-    ax2.legend()
+    # ax2.plot(range(len(ga_slover.record_error)), ga_slover.record_error, label='Error')
+    # ax2.set_xlabel('Iteration')
+    # ax2.set_ylabel('Error')
+    # ax2.legend()
 
-    plt.tight_layout()
-    plt.show()
+    # plt.tight_layout()
+    # plt.show()
 if __name__ == "__main__":
     main()
